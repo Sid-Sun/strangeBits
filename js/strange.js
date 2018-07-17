311ec5e4300b796a9ca8112c4f89afa9222a17ea
Sid-Sun <sid-sun@outlook.com>
2018-07-17 18:14:37 +0530

    Added Google Analytics properly && Made the footer a jumbotron so it is more responsive :)
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

---
+++ js/strange.js
@@ -0,0 +1,13 @@
+console.log("Hello world!");
+function enableGA() {
+    let script = document.createElement('script');
+    script.setAttribute('src', 'js/analytics.js');
+    script.setAttribute('type', 'text/javascript');
+    document.getElementsByTagName('head')[0].appendChild(script);
+    console.log("Google Analytics has been enabled");
+    document.getElementById("pop").style.display = "none";
+}
+function disableGA() {
+    console.log("Google Analytics was NOT enabled");
+    document.getElementById("pop").style.display = "none";
+}
