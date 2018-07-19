eb64979726993db54868dab054c2ce4260de53a6
ZerNico <ZerNico.yt@gmail.com>
2018-07-19 18:32:01 +0200

    Add cookie control

--- js/strange.js
+++ js/strange.js
@@ -1,4 +1,15 @@
 console.log("Hello world!");
+checkGA();
+
+function checkGA() {
+    var analytics = getCookie('analytics')
+    if (analytics == 'enabled') {
+        enableGA()
+     } else if (analytics == 'disabled') {
+        disableGA();
+    }
+}
+
 function engageGA() {
     window.dataLayer = window.dataLayer || [];
     function gtag() {
@@ -7,6 +18,7 @@ function engageGA() {
     gtag('js', new Date());
     gtag('config', 'UA-121801387-2');
 }
+
 function enableGA() {
     let script = document.createElement('script');
     script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-121801387-2');
@@ -15,11 +27,42 @@ function enableGA() {
     engageGA();
     console.log("Google Analytics has been enabled");
     document.getElementById("pop").style.display = "none";
+    var analytics = getCookie('analytics')
+    if (analytics == null) {
+        setCookie('analytics', 'enabled', 30);     
+    }
 }
+
 function disableGA() {
     console.log("Google Analytics was NOT enabled");
     document.getElementById("pop").style.display = "none";
+    var analytics = getCookie('analytics')
+    if (analytics == null) {
+        setCookie('analytics', 'disabled', 30);     
+    }
 }
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
 if(navigator.doNotTrack == 1) {
     disableGA();
 }
