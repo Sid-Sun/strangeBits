b63d612c90e5d6a0b45bb29e5d08aa8fee1e3c4c
Sid-Sun <sid-sun@outlook.com>
2018-07-19 17:33:21 +0530

    Remove custom analytics JS, switch to strange.min.js, disable Analytics on privacy policy page.
    Adding custom analytics JS is inefficent cu it'll need to be pulled from our comparatitivelty slow servers than Google's and it'll save bandwidth,
    
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

--- js/strange.js
+++ js/strange.js
@@ -1,9 +1,18 @@
 console.log("Hello world!");
+function engageGA() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', 'UA-121801387-2');
+}
 function enableGA() {
     let script = document.createElement('script');
-    script.setAttribute('src', 'js/analytics.js');
+    script.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=UA-121801387-2');
     script.setAttribute('type', 'text/javascript');
     document.getElementsByTagName('head')[0].appendChild(script);
+    engageGA();
     console.log("Google Analytics has been enabled");
     document.getElementById("pop").style.display = "none";
 }
@@ -11,7 +20,6 @@ function disableGA() {
     console.log("Google Analytics was NOT enabled");
     document.getElementById("pop").style.display = "none";
 }
-
 if(navigator.doNotTrack == 1) {
     disableGA();
 }
