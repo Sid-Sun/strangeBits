37d2fa7a23cabe488460753523ed051464b95957
Sid-Sun <sid-sun@outlook.com>
2018-08-31 12:23:07 +0530

    Make the links into the div itself, switch to automationDB.min.js, update privacy policy for third party disclosures.
    Making an element for each card and injecting in the onclick event is not ideal so say the least.
    
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

--- js/automationDB.js
+++ js/automationDB.js
@@ -19,18 +19,17 @@ someTest = [
     },
 ];
 for(i=0;i<someTest.length;i++) {
-    createCards(someTest[i].title,someTest[i].subject,someTest[i].img);
-   clickOn(someTest[i].title,someTest[i].location);
-}
+    createCards(someTest[i].title,someTest[i].subject,someTest[i].img,someTest[i].location);
+  /* clickOn(someTest[i].title,);
+*/}
 
-function createCards(cardTitle,cardText,cardImg) {
+function createCards(cardTitle,cardText,cardImg,redLocation) {
     $("#cards-div").append( "" +
-        "<div class='card col-md-4' id='"+cardTitle+"'style='cursor: pointer;'>" + "<img class='card-img-top' src='img/" + cardImg + "' alt='Card image'>" + "<div class='card-body'>" + "<h5 class='card-title'>" + cardTitle + "</h5>" + "<p class='card-text'>" + cardText + "</p>" + "</div>" + "</div>")
+        "<div onclick=location.href='" + redLocation +"' class='card col-md-4' id='"+cardTitle+"'style='cursor: pointer;'>" + "<img class='card-img-top' src='img/" + cardImg + "' alt='Card image'>" + "<div class='card-body'>" + "<h5 class='card-title'>" + cardTitle + "</h5>" + "<p class='card-text'>" + cardText + "</p>" + "</div>" + "</div></a>")
 }
 function clickOn(cardID,position) {
     console.log("Adding");
-    let funnyScript = document.createElement('script');
-    let funnyText = document.createTextNode("document.getElementById('" + cardID + "').onclick = function () { location.href = '"+position+"'}");
+    let funnyScript = document.getElementById('custom-internal-script');
+    let funnyText = document.createTextNode("document.getElementById('" + cardID + "').onclick = function () { location.href = '"+position+"'};");
     funnyScript.appendChild(funnyText);
-    document.getElementsByTagName('head')[0].appendChild(funnyScript);
 }
